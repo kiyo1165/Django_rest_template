98655e728598c5aa4f007c5dcf2e53033a68a02d
Quest_love33 <46390210+kiyo1165@users.noreply.github.com>
2022-01-05 03:30:57 +0900

    Update models.py
    変更前： objects = UserManager
    変更後： objects = UserManager()

--- core/models.py
+++ core/models.py
@@ -37,7 +37,7 @@ class User(AbstractBaseUser, PermissionsMixin):
     is_active = models.BooleanField(default=True)
     is_staff = models.BooleanField(default=False)
 
-    objects = UserManager
+    objects = UserManager()
 
     # デフォルトの入力をemailに変更
     USERNAME_FIELD = 'email'
